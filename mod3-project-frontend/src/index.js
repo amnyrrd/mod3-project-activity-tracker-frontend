a8287ca35907962ea16692fced72ba1946588935
SCDan0624 <dmayle1@hotmail.com>
2020-03-23 17:18:52 -0700

    Added fetch data with seed names showing

--- mod3-project-frontend/src/index.js
+++ mod3-project-frontend/src/index.js
@@ -1,6 +1,37 @@
-const input= document.getElementById('login_input')
+document.addEventListener('DOMContentLoaded', () => {
 
-console.log(input);
+    const form = document.getElementById("login_form");
+    const div2 = document.getElementById("seed")
+
+    console.log(div2)
+
+
+
+    // console.log(form);
+
+
+    // fetch the seed data
+    fetch(`http://localhost:3000/users`)
+    .then(resp => resp.json())
+    .then(data => renderSeed(data))
+
+    
+    function renderSeed(seeds){
+        console.log(seeds)
+        seeds.forEach(seed => {
+            const li = document.createElement('li')
+            div2.append(li)
+            li.innerText = seed.name
+
+        })
+    }
+
+
+
+
+    
+
+});
 
 
 
